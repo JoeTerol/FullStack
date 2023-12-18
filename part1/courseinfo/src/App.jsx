a4d4d6d7484d54e6164f9febd1b8ce88aa04600c
JoeTerol <teroljoseeduardo@gmail.com>
2023-12-18 12:15:47 -0300

    refactor: exercise 1.5 done

--- part1/courseinfo/src/App.jsx
+++ part1/courseinfo/src/App.jsx
@@ -4,7 +4,7 @@ const Header = (props) => {
   return (
     <>
       <h1>
-        {props.title}
+        {props.name}
       </h1>
     </>
   )
@@ -21,7 +21,7 @@ const Content = (props) => {
 }
 const Part = (props) => {
   return (
-    <p>{props.name} {props.exercise} </p>
+    <p>{props.name} {props.exercises} </p>
   )
 }
 const Total = (props) => {
@@ -37,27 +37,28 @@ const Total = (props) => {
 
 
 const App = () => {
-  const course = 'Half Stack application development'
-  const parts =[ 
-  {
+  const course = {
+   name: 'Half Stack application development',
+   parts: [ 
+    {
     name: 'Fundamentals of React',
     exercises: 10
-  },
+    },
     {
     name: 'Using props to pass data',
     exercises: 7
-  },
+    },
    {
     name: 'State of a component',
     exercises: 14
-  }
-]
-
+    }
+  ]
+}
   return (
     <>
     <Header title={course}/>
-    <Content parts={parts} />
-    <Total parts={parts} />
+    <Content parts={course.parts} />
+    <Total parts={course.parts} />
     </>
 
   )
